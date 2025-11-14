d971a2319f0ce6d74e68b3daa4ee1da3476f6d26
Marc Sutthaloung <marc.sutthaloung@gmx.net>
2025-11-14 14:38:41 +0100

    feat: use user acceleration (no gravity)

--- lib/services/shake_detector_service.dart
+++ lib/services/shake_detector_service.dart
@@ -5,40 +5,46 @@ import 'dart:async';
 import '../constants/alarm_constant.dart';
 
 class ShakeDetectorService {
-  StreamSubscription? _subscription;
-  final Stream<AccelerometerEvent> _accelerometerStream;
+  StreamSubscription<UserAccelerometerEvent>? _subscription;
+  final Stream<UserAccelerometerEvent> _accelerometerStream;
   double _shakeThreshold = AlarmConstants.shakeThresholdLight;
   DateTime? _lastShakeTime;
   static const _shakeCooldown = AlarmConstants.shakeCooldown;
 
-  ShakeDetectorService({Stream<AccelerometerEvent>? accelerometerStream})
-    : _accelerometerStream = accelerometerStream ?? accelerometerEvents;
+  ShakeDetectorService({Stream<UserAccelerometerEvent>? accelerometerStream})
+      : _accelerometerStream =
+            accelerometerStream ?? userAccelerometerEventStream();
 
-  void startListening(Function() onShake, {double threshold = AlarmConstants.shakeThresholdLight}) {
+  void startListening(Function() onShake,
+      {double threshold = AlarmConstants.shakeThresholdLight}) {
+
+    if (_subscription != null) return;
+
     _shakeThreshold = threshold;
     _lastShakeTime = null;
 
-    _subscription = _accelerometerStream.listen((AccelerometerEvent event) {
+    _subscription = _accelerometerStream.listen((event) {
       final now = DateTime.now();
 
-      if (_lastShakeTime != null &&
-          now.difference(_lastShakeTime!) < _shakeCooldown) {
+      if (_lastShakeTime != null && now.difference(_lastShakeTime!) < _shakeCooldown) {
         return;
       }
 
-      double magnitude = sqrt(
-        event.x * event.x + event.y * event.y + event.z * event.z,
-      );
+      final magnitude = sqrt(event.x * event.x + event.y * event.y + event.z * event.z);
 
       if (magnitude > _shakeThreshold) {
         _lastShakeTime = now;
-        onShake();
+        try {
+          onShake();
+        } catch (_) {}
       }
+    }, onError: (e) {
     });
   }
 
   void stopListening() {
     _subscription?.cancel();
+    _subscription = null;
     _lastShakeTime = null;
   }
 }
